81f36c390892a9c7221f58da6f23422bb1020e33
brosmar18 <bryangonzalez1896@icloud.com>
2024-02-21 22:15:08 -0600

    Navbar added, with mobile nav

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,8 +1,52 @@
-import React from 'react'
+import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { IconMenu2 } from '@tabler/icons-react';
+import { IconX } from '@tabler/icons-react';
+import { motion } from 'framer-motion';
 
 const Navbar = () => {
+    const [toggle, setToggle] = useState(false);
     return (
-        <div>Navbar</div>
+        <div className='nav'>
+            <div className='logo'>
+
+            </div>
+            <ul className='nav__links'>
+                {['home', 'tasks', 'calendar', 'account'].map((link) => (
+                    <li className='nav__link' key={`link-${link}`}>
+                        <div />
+                        <NavLink to={link === 'home' ? '/' : `/${link}`}>
+                            {link}
+                        </NavLink>
+                    </li>
+                ))}
+            </ul>
+            <div className='nav__mobile'>
+                <IconMenu2 onClick={() => setToggle(true)} />
+
+                {toggle && (
+                    <motion.div
+                        whileInView={{ x: [300, 0] }}
+                        transition={{ duration: 0.85, ease: 'easeOut' }}
+                    >
+                        <IconX onClick={() => setToggle(false)} />
+                        <ul>
+                            {['home', 'tasks', 'calendar', 'account'].map((link) => (
+                                <li className='nav__link' key={`link-${link}`}>
+                                    <div />
+                                    <NavLink
+                                        to={link === 'home' ? '/' : `/${link}`}
+                                        onClick={() => setToggle(false)}
+                                    >
+                                        {link}
+                                    </NavLink>
+                                </li>
+                            ))}
+                        </ul>
+                    </motion.div>
+                )}
+            </div>
+        </div>
     )
 }
 
