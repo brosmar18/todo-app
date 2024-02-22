35386da1cc0c8830b3926ffcadae189b8bd7c097
brosmar18 <bryangonzalez1896@icloud.com>
2024-02-21 23:01:27 -0600

    Footer added

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { logo } from '../../assets';
+import { IconBrandTwitter, IconBrandYoutube, IconBrandInstagram } from '@tabler/icons-react';
+import './Footer.scss';
 
 const Footer = () => {
     return (
-        <div>Footer</div>
-    )
-}
+        <footer className='footer'>
+            <img src={logo} alt='logo' />
+            <div className='footer__socials'>
+                <IconBrandInstagram size={20} />
+                <IconBrandTwitter size={20} />
+                <IconBrandYoutube size={20} />
+            </div>
+        </footer>
+    );
+};
 
-export default Footer
+export default Footer;
