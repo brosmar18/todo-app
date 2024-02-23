e30160b555c92d7f4f7e73addfc08e638dee2858
brosmar18 <bryangonzalez1896@icloud.com>
2024-02-22 19:50:55 -0600

    Context syntax error fixed

--- src/context/Settings/index.jsx
+++ src/context/Settings/index.jsx
@@ -1,17 +1,19 @@
 import React, { createContext, useState } from 'react';
 
-export const SetingsContext = createContext({
+const defaultSettings = {
     displayLimit: 3,
     hideCompleted: true,
     sortField: 'difficulty'
-});
-
-const Settings = ({ children }) => {
-    return (
-        <SetingsContext.Provider value={settings}>
-            {children}
-        </SetingsContext.Provider>
-    );
 };
 
-export default Settings;
+export const SettingsContext = createContext(defaultSettings);
+
+export const SettingsProvider = ({ children }) => {
+    const [settings, setSettings] = useState(defaultSettings);
+
+    return (
+        <SettingsContext.Provider value={settings}>
+            {children}
+        </SettingsContext.Provider>
+    );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
-import App from './App.jsx'
+import { SettingsProvider } from './context/Settings';
+import App from './App';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <MantineProvider>
-      <App />
+      <SettingsProvider>
+        <App />
+      </SettingsProvider>
     </MantineProvider>
   </React.StrictMode>,
 )
