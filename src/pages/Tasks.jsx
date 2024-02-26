8c3a80c164e229a2cf486ad92465d7298516784a
brosmar18 <bryangonzalez1896@icloud.com>
2024-02-26 14:46:18 -0600

    Task card designed

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -2,7 +2,7 @@ import List from '../Components/List';
 
 const Tasks = () => {
   return (
-    <div className='w-full h-full bg-blue-300 p-4'>
+    <div className='w-full h-ful p-4'>
       <List />
     </div>
   )
