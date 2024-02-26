986d1bc4d74f99530eea8f6e3ecc5625f677c930
brosmar18 <bryangonzalez1896@icloud.com>
2024-02-26 14:09:10 -0600

    Task added successfully modal added

--- src/Components/HomeComponents/AddTask/index.jsx
+++ src/Components/HomeComponents/AddTask/index.jsx
@@ -2,12 +2,13 @@ import useForm from "../../../hooks/form";
 import { v4 as uuid } from 'uuid';
 import { useTasks } from "../../../context/TaskContext";
 import { useState } from "react";
-import { NumberInput } from "@mantine/core";
+import { NumberInput, Modal } from "@mantine/core";
 
 const AddTask = () => {
     const { addTask } = useTasks();
     const [defaultValues] = useState({ difficulty: 4 });
     const { handleChange, handleSubmit: handleFormSubmit } = useForm(submitForm, defaultValues);
+    const [openModal, setOpenModal] = useState(false);
 
     function submitForm(values) {
         addTask({
@@ -15,11 +16,20 @@ const AddTask = () => {
             id: uuid(),
             complete: false,
         });
+        setOpenModal(true);
     }
 
 
     return (
         <div className="bg-white p-6 rounded-2xl shadow-md flex flex-col items-center space-y-4 row-start-5 md:row-start-3 md:col-start-1 md:col-span-2 lg:row-start-1 lg:col-start-4 lg:col-span-2">
+            <Modal
+                opened={openModal}
+                onClose={() => setOpenModal(false)}
+                title="Success!"
+                centered
+            >
+                <p>Task has been successfully added!</p>
+            </Modal>
             <div className="px-6 py-4">
                 <p className="font-bold text-2xl mb-2">Add Task</p>
                 <div className="border-b-2 border-gray-100 mb-2" />
@@ -59,7 +69,7 @@ const AddTask = () => {
                 </fieldset>
                 <button className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">Add Item</button>
             </form>
-        </div>
+        </div >
     )
 }
 
