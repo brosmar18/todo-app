b61c7c88d564735412b204d5538a85f3766f8be3
brosmar18 <bryangonzalez1896@icloud.com>
2024-02-25 18:29:54 -0600

    Grid system updated to be responsive

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,16 @@
 const Home = () => {
   return (
-    <div className="grid grid-cols-2 md:grid-cols-[1fr,2fr,2fr] grid-rows-[repeat(3,minmax(0,1fr))] gap-5 bg-gray-200 w-full px-4 sm:px-6 md:px-8 lg:px-10 xl:px-20">
-      <div className="bg-red-500 h-32 row-start-1 col-span-2 md:col-span-1 md:h-auto"> Div 1</div>
-      <div className="bg-green-500 h-32 row-start-2 col-span-2 md:col-span-1 md:h-auto"> Div 2</div>
-      <div className="bg-blue-500 h-32 row-start-3 col-span-2 md:col-start-1 md:col-end-3 md:h-auto"> Div 3</div>
-      <div className="bg-yellow-500 h-32 row-start-4 col-span-2 md:row-start-1 md:col-start-2 md:col-end-3 md:h-auto"> Div 4</div>
-      <div className="bg-purple-500 h-32 row-start-5 col-span-2 md:row-start-2 md:col-start-2 md:col-end-3 md:h-auto"> Div 5</div>
-      <div className="bg-orange-500 h-[96px] md:h-auto row-start-6 col-span-2 md:row-start-1 md:col-start-3 md:row-span-3"> Div 7</div>
+    <div className="flex flex-col sm:grid sm:grid-cols-[1fr,repeat(2,_2fr)] sm:grid-rows-[repeat(2,_1fr)] sm:gap-0 w-full">
+      {/* Box 2 */}
+      <div className="bg-green-300 flex items-center justify-center sm:col-start-1 sm:col-end-2 sm:row-start-1 sm:row-end-2">Box 2</div>
+      {/* Box 3 */}
+      <div className="bg-blue-300 flex items-center justify-center sm:col-start-1 sm:col-end-2 sm:row-start-2 sm:row-end-3">Box 3</div>
+      {/* Box 4 */}
+      <div className="bg-yellow-300 flex items-center justify-center sm:col-start-2 sm:col-end-3 sm:row-start-1 sm:row-end-2">Box 4</div>
+      {/* Box 5 */}
+      <div className="bg-purple-300 flex items-center justify-center sm:col-start-2 sm:col-end-3 sm:row-start-2 sm:row-end-3">Box 5</div>
+      {/* Box 6 */}
+      <div className="bg-pink-300 flex items-center justify-center sm:col-start-3 sm:col-end-4 sm:row-start-1 sm:row-end-3">Box 6</div>
     </div>
   );
 };
