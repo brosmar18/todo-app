8eba2cb591773e3ded5793a7d9bbbcc550e04c58
brosmar18 <bryangonzalez1896@icloud.com>
2024-02-26 13:54:59 -0600

    add task form working. Adding item to List of tasks

--- src/Components/HomeComponents/AddTask/index.jsx
+++ src/Components/HomeComponents/AddTask/index.jsx
@@ -2,11 +2,12 @@ import useForm from "../../../hooks/form";
 import { v4 as uuid } from 'uuid';
 import { useTasks } from "../../../context/TaskContext";
 import { useState } from "react";
+import { NumberInput } from "@mantine/core";
 
 const AddTask = () => {
     const { addTask } = useTasks();
-    const { defaultValues } = useState({ difficulty: 4 });
-    const { handleChange, handleSubmit: handleFormSumbmit } = useForm(submitForm, defaultValues);
+    const [defaultValues] = useState({ difficulty: 4 });
+    const { handleChange, handleSubmit: handleFormSubmit } = useForm(submitForm, defaultValues);
 
     function submitForm(values) {
         addTask({
@@ -23,18 +24,38 @@ const AddTask = () => {
                 <p className="font-bold text-2xl mb-2">Add Task</p>
                 <div className="border-b-2 border-gray-100 mb-2" />
             </div>
-            <form className="w-full">
+            <form className="w-full" onSubmit={(e) => handleFormSubmit(e)}>
                 <fieldset className="mb-4">
                     <label className="block text-gray-700 text-sm font-bold mb-2">Task</label>
-                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="text" type="text" placeholder="Item Details" />
+                    <input
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        name="text"
+                        type="text"
+                        placeholder="Item Details"
+                        onChange={handleChange}
+                    />
                 </fieldset>
                 <fieldset className="mb-4">
                     <label className="block text-gray-700 text-sm font-bold mb-2">Assigned to:</label>
-                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="assignee" type="text" placeholder="Assignee Name" />
+                    <input
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        name="assignee"
+                        type="text"
+                        placeholder="Assignee Name"
+                        onChange={handleChange}
+                    />
                 </fieldset>
                 <fieldset className="mb-4">
                     <label className="block text-gray-700 text-sm font-bold mb-2">Difficulty</label>
-                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="difficulty" type="range" min="1" max="5" />
+                    <NumberInput
+                        name="difficulty"
+                        label="Difficulty"
+                        placeholder="Choose 1 - 5"
+                        min={1}
+                        max={5}
+                        onChange={handleChange}
+                        defaultValue={defaultValues.difficulty}
+                    />
                 </fieldset>
                 <button className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">Add Item</button>
             </form>
