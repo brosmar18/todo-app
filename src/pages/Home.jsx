e1d98e372f04555aa7e9fe662a7b4bc52c6aee57
brosmar18 <bryangonzalez1896@icloud.com>
2024-03-20 13:04:04 -0500

    Home page updated to display name and role dynamically

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import Card from "../Components/Card";
+import { AuthContext } from "../context/AuthContext";
 
 const Home = () => {
+  const { user } = useContext(AuthContext);
   return (
     <div className="flex-1 p-6">
       <div className="mb-6">
-        <h1 className="text-4xl font-bold text-gray-800">Welcome, Joe!</h1>
-        <p className="mt-2 text-lg text-gray-600">Software Developer</p>
+        <h1 className="text-4xl font-bold text-gray-800">
+          Welcome, <span>{user.firstName}</span>
+        </h1>
+        <p className="mt-2 text-lg text-gray-600">{user.role}</p>
       </div>
       <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
         <Card
